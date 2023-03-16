b858057fb221c6c68c86ff1ced4f691e41c5492f
garcondeau <garcondeau@icloud.com>
2023-03-16 18:51:50 -0500

    Enabled debug mode

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def index():
     return 'Hello World!'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
